c41aa90a9e95358e83e722c2aed42316a980e988
Carlo Landmeter <clandmeter@alpinelinux.org>
2017-12-08 08:40:13 +0000

    only close stream when its open

--- generate-json.lua
+++ generate-json.lua
@@ -58,7 +58,7 @@ function get_index_status(uri)
 	local res = {}
 	local status, modified
 	local headers, stream = request.new_from_uri(uri):go(conf.http_timeout)
-	stream:shutdown()
+	if stream then stream:shutdown() end
 	if headers then 
 		status = headers:get(":status")
 	else
