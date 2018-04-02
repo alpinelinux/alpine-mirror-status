a4f6e021b09b10fa8ad4b4c222dc7ba75ad81b5b
Carlo Landmeter <clandmeter@alpinelinux.org>
2018-04-02 20:50:24 +0000

    add mirrors.txt support

--- generate-mirrors-json.lua
+++ generate-mirrors-json.lua
@@ -12,18 +12,27 @@ local yaml = require("lyaml")
 local pfile = require("pl.file")
 
 local input  = assert(arg[1], "Please provide input yaml as first argument!")
-local output = assert(arg[2], "Please provide output json as second argument!")
+local output = assert(arg[2], "Please provide output directory as second argument!")
 
 local mirrors = yaml.load(pfile.read(input))
 local public_keys = { "name", "location", "bandwidth", "urls" }
 
 local res = {}
+local txt = {}
 
 for k,m in ipairs(mirrors) do
 	res[k] = {}
 	for _,pk in ipairs(public_keys) do
 		res[k][pk] = m[pk]
+		if pk == "urls" then
+			for _,url in pairs(m.urls) do
+				if url:find("http://") then
+					table.insert(txt, url)
+				end
+			end
+		end
 	end
 end
 
-pfile.write(output, json.encode(res))
+pfile.write(output.."/mirrors.json", json.encode(res))
+pfile.write(output.."/mirrors.txt", table.concat(txt, "\n"))
